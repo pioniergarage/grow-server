96106bdd0b60d0c15f1cd40975e918729039e931
Dominik Doerner <nik_doerner@web.de>
2019-04-02 22:18:26 +0200

    Added and integrated a design template for the admin area

---
+++ Grow.Server/Areas/Admin/Controllers/AccountController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Grow.Server.Model;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Grow.Server.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class AccountController : Controller
+    {
+        private readonly GrowDbContext _context;
+
+        public AccountController(GrowDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Logout()
+        {
+            return RedirectToAction("Index", "Home", new { Area = "" });
+        }
+    }
+}

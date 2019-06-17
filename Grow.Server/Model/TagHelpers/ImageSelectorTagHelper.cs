94546480d4fafb3f131d08cc541ed458c3e5a98c
Dominik Doerner <nik_doerner@web.de>
2019-06-17 16:42:00 +0200

    Added client side script to upload images via ajax (server side still missing)

---
+++ Grow.Server/Model/TagHelpers/ImageSelectorTagHelper.cs
@@ -0,0 +1,163 @@
+﻿using Grow.Data.Entities;
+using Microsoft.AspNetCore.Html;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.TagHelpers;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text.Encodings.Web;
+using System.Threading.Tasks;
+
+namespace Grow.Server.Model.TagHelpers
+{
+    [HtmlTargetElement("imgselector", Attributes = ForAttributeName, TagStructure = TagStructure.WithoutEndTag)]
+    public class ImageSelectorTagHelper : TagHelper
+    {
+        private const string ItemsAttributeName = "asp-items";
+        private const string ForAttributeName = "asp-for";
+        private const string CategoryAttributeName = "asp-category";
+        private const string WrapperClassName = "img-selector";
+
+        [HtmlAttributeName(ForAttributeName)]
+        public ModelExpression For { get; set; }
+
+        [HtmlAttributeName(ItemsAttributeName)]
+        public ICollection<SelectListItem> Items { get; set; }
+
+        [HtmlAttributeName(CategoryAttributeName)]
+        public string Category { get; set; }
+        
+        public string Class { get; set; }
+
+        public string Style { get; set; }
+
+        public IHtmlGenerator Generator { get; }
+
+        [HtmlAttributeNotBound]
+        [ViewContext]
+        public ViewContext ViewContext { get; set; }
+
+        public string Id { get; }
+
+        public ImageSelectorTagHelper(IHtmlGenerator generator)
+        {
+            Generator = generator;
+            Id = new Random().Next(1000, 9999).ToString();
+        }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            // Check input
+            if (context == null)
+                throw new ArgumentNullException(nameof(context));
+            if (output == null)
+                throw new ArgumentNullException(nameof(output));
+
+            // output config
+            output.TagName = "div";
+            output.TagMode = TagMode.StartTagAndEndTag;
+
+            // build tags
+            var select = CreateSelectElement(context);
+            var file = CreateFileElement(context);
+            var btn = CreateButtonElement(context);
+            var wrapper = WrapInDiv(new[] { select, file, btn });
+
+            // Pass-through attributes to select
+            if (Class != null)
+                wrapper.Attributes.Add("class", Class);
+            if (Style != null)
+                wrapper.Attributes.Add("style", Style);
+
+            // Other attributes
+            wrapper.AddCssClass(WrapperClassName);
+            wrapper.Attributes.Add("name", For.Name);
+            wrapper.Attributes.Add("value", For.Model?.ToString() ?? "");
+
+            // set content
+            output.Content.SetHtmlContent(wrapper);
+        }
+
+        private TagBuilder WrapInDiv(IEnumerable<IHtmlContent> elements)
+        {
+            TagBuilder div = new TagBuilder("div");
+            foreach (IHtmlContent element in elements)
+            {
+                div.InnerHtml.AppendHtml(element);
+            }
+            return div;
+        }
+
+        private IHtmlContent CreateSelectElement(TagHelperContext context)
+        {
+            var selectTagHelper = new SelectTagHelper(Generator)
+            {
+                For = For,
+                Items = Items,
+                ViewContext = ViewContext,
+            };
+
+            TagHelperOutput selectOutput = CreateTagHelperOutput("select");
+            selectTagHelper.Process(context, selectOutput);
+
+            selectOutput.AddClass("form-control", HtmlEncoder.Default);
+            selectOutput.Attributes.Add("id", $"file-selector-{Id}");
+
+            return selectOutput;
+        }
+
+        private IHtmlContent CreateFileElement(TagHelperContext context)
+        {
+            var input = new TagBuilder("input");
+            input.Attributes.Add("type", "file");
+            input.Attributes.Add("id", $"file-upload-{Id}");
+            input.Attributes.Add("style", "display: none");
+            input.Attributes.Add("dat-category", Category);
+            input.Attributes.Add("dat-output", $"file-selector-{Id}");
+            return input;
+        }
+
+        private IHtmlContent CreateButtonElement(TagHelperContext context)
+        {
+            var buttonTagHelper = new AnchorTagHelper(Generator)
+            {
+                Action = "Create",
+                Controller = "File",
+                RouteValues =
+                {
+                    { "category", Category }
+                },
+                ViewContext = ViewContext
+            };
+
+            TagHelperOutput buttonOutput = CreateTagHelperOutput("a");
+            buttonTagHelper.Process(context, buttonOutput);
+
+            buttonOutput.AddClass("form-control", HtmlEncoder.Default);
+            buttonOutput.Attributes.Add("target", "_blank");
+            buttonOutput.Content.SetHtmlContent("<i class=\"fa fa-upload\"></i>");
+
+            var fileInputId = "file-upload-" + Id;
+            var script = string.Format("document.getElementById('{0}').click(); return false;", fileInputId);
+            buttonOutput.Attributes.Add("onclick", script);
+
+            return buttonOutput;
+        }
+
+        private TagHelperOutput CreateTagHelperOutput(string tagName)
+        {
+            return new TagHelperOutput(
+                tagName: tagName,
+                attributes: new TagHelperAttributeList(),
+                getChildContentAsync: (_, __) =>
+                {
+                    return Task.Factory.StartNew<TagHelperContent>(
+                            () => new DefaultTagHelperContent());
+                }
+            );
+        }
+    }
+}
--- Grow.Server/wwwroot/js/backend.js
+++ Grow.Server/wwwroot/js/backend.js
@@ -1,17 +1,32 @@
-﻿$(document).ready(function () {
+﻿// MAGIC NUMBERS / STRINGS
+url_select_year            = "/Admin/Home/SetYear";
+url_create_file_prefix     = "/Admin/Api/File?category=";
+url_search_entities_prefix = "/Admin/Api/";
+var debug;
 
-    // rig up contest selector
+// ADD GLOBAL EVENT HANDLERS
+$(document).ready(function () {
+
+    // rig up contest selector (in Admin navbar)
     var selector = $("#contest-selector");
     selector.change(function () {
         var newValue = selector.children("option:selected").val();
-        $.post("/Admin/Home/SetYear", { year: newValue }, function (data) {
+        $.post(url_select_year, { year: newValue }, function (data) {
             location.reload();
         });
     });
+
+    // image uploader
+    $(".img-selector input[type=file]").change(create_image_upload_function(this));
+
+    // entity search
+    // TODO
 });
 
+// FUNCTION DEFINITIONS
+
 /**
- * Creates an event handler function for searching entities that can be assigned to a javascript event.
+ * Creates a (key) event handler function for searching entities that can be assigned to a javascript event.
  * 
  * The event handler will take the value of the linked DOM object and send an AJAX request 
  * for entities of the given type (e.g. Event) that contain the search string in their name.
@@ -21,21 +36,27 @@
  * The returned results are displayed in as <li> elements inside the given output DOM element.
  * 
  * When one of the <li> elements is clicked, the form on the same page is filled with data from the chosen element.
+ * 
+ * @param {string} type Entity type to search for
+ * @param {string} output_id ID of the element in which to display the search results
+ * @returns {Function} Handler function that fetches and displays entities
  */
-var debug;
 function create_search_entity_function(type, output_id) {
 
     var search_timer;
-    var url = "/Admin/Api/" + type;
+    var url = url_search_entities_prefix + type;
     var output_element = $("#" + output_id);
-    
+
+    // Return a handler function
     return (event) => {
         var input_element = event.target;
         var searchString = $(input_element).val();
         var timestamp = $.now();
 
+        // function handler when search result item is selected
         var on_element_chosen = (elem) => {
             $("input, select").each((index, input) => {
+                // fill values on current form from selected entity
                 var name = $(input).attr("name");
                 if (!name) return;
                 name = name.substring(0, 1).toLowerCase() + name.substring(1);
@@ -51,19 +72,21 @@ function create_search_entity_function(type, output_id) {
             });
         };
 
+        // function to process search results
         var on_success = (data) => {
-
-            debug = data;
             $(input_element).removeClass("loading");
 
+            // versioning of results to avoid older requests to overwrite newer ones
             var output_version = output_element.attr("output-version");
             if (output_version !== undefined && output_version > timestamp)
                 return;
 
+            // base setup for output element
             $(output_element).show();
             output_element.attr("output-version", timestamp);
             output_element.html("<li disabled>-- Select or search again --</li>");
 
+            // add search results (as <li>)
             data.forEach(elem => {
                 var text = elem.name;
                 text += " (created at " + elem.createdAt.substring(0, 10) + ")";
@@ -74,6 +97,7 @@ function create_search_entity_function(type, output_id) {
             });
         };
 
+        // error function
         var on_fail = () => {
             $(input_element).removeClass("loading");
             var text = "Error fetching entities via AJAX";
@@ -81,11 +105,71 @@ function create_search_entity_function(type, output_id) {
             output_element.append("<li>" + text + "</li>");
         };
 
+        // delayed ajax to execute search (to avoid constant updates while typing)
         clearTimeout(search_timer);
         search_timer = setTimeout(() => {
             $(input_element).addClass("loading");
             $.get(url, { search: searchString }, on_success)
                 .fail(on_fail);
         }, 2000);
+    };
+}
+
+/**
+ * Creates a (change) event handler function for uploading files to the server.
+ *
+ * The event handler will take the value of the linked input[type=file] and send an AJAX request
+ * to upload the selected type in a given category (e.g. TeamLogos).
+ *
+ * The class "loading" will be assigned to the linked DOM object while the ajax request runs.
+ *
+ * The create file is (optionally) displayed as a <option> element inside a given output <select> element.
+ * 
+ * The attached input element needs to contain the attributes "dat-category" and "dat-output".
+ * 
+ * @returns {Function} Handler function that uploads file and adds result to given <select>
+ */
+function create_image_upload_function() {
+    
+    return (event) => {
+        var input_element = event.target;
+        var category = $(input_element).attr("dat-category");
+        var output_element = $($(input_element).attr("dat-output"));
+        
+        var files = input_element.files;
+        var formData = new FormData();
+        for (var i = 0; i < files.length; i++) {
+            formData.append("fileData", files[i]);
+        }
+
+        var on_success = (data) => {
+            $(input_element).removeClass("loading");
+
+            if (output_element) {
+                var option = document.createElement("option");
+                option.innerHTML = data.name;
+                option.value = data.id;
+                output_element.append(option);
+            }
+        };
+
+        var on_fail = () => {
+            $(input_element).removeClass("loading");
+
+            var text = "Error fetching entities via AJAX";
+            console.log(text);
+        };
+        
+        $(input_element).addClass("loading");
+        $.ajax({
+            url: url_create_file_prefix + category,
+            type: "POST",
+            dataType: "json",
+            data: formData,
+            contentType: false,
+            processData: false,
+            sucess: on_success,
+            error: on_fail
+        });
     };
 }

11554d2619e87e8599afc9c2db67526ccf1aebbf
Dominik Doerner <nik_doerner@web.de>
2020-01-09 16:54:32 +0100

    Extracted countdown on front page into a taghelper + js plugin

---
+++ Grow.Server/Model/TagHelpers/CountdownTagHelper.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.AspNetCore.Html;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Grow.Server.Model.TagHelpers
+{
+    [HtmlTargetElement("countdown", Attributes = TargetTimeAttributeName, TagStructure = TagStructure.WithoutEndTag)]
+    public class CountdownTagHelper : BaseTagHelper
+    {
+        private const string TargetTimeAttributeName = "asp-datetime";
+
+        [HtmlAttributeName(TargetTimeAttributeName)]
+        public DateTime TargetTime { get; set; }
+
+        public CountdownTagHelper(IHtmlGenerator generator) : base(generator, "div", TagMode.StartTagAndEndTag)
+        {
+        }
+
+        protected override IHtmlContent CreateHtmlContent(TagHelperContext context)
+        {
+            if (TargetTime < DateTime.Now)
+                return null;
+
+            var timeLeft = TargetTime - DateTime.Now;
+
+            var days = CreateSection(timeLeft, CountdownSection.Days);
+            var hours = CreateSection(timeLeft, CountdownSection.Hours);
+            var mins = CreateSection(timeLeft, CountdownSection.Minutes);
+            var secs = CreateSection(timeLeft, CountdownSection.Seconds);
+
+            return WrapInDiv(new[] { days, hours, mins, secs }, "clockdiv");
+        }
+        
+        private IHtmlContent CreateSection(TimeSpan timeLeft, CountdownSection section)
+        {
+            var content = new HtmlContentBuilder();
+
+            var number = new TagBuilder("span");
+            number.AddCssClass(section.Identifier);
+            number.InnerHtml.Append(section.TimeSelector(timeLeft).ToString(section.FormatString));
+
+            var label = new TagBuilder("div");
+            label.AddCssClass("smalltext");
+            label.InnerHtml.Append(section.DisplayName);
+
+            return WrapInDiv(new[] { number, label });
+        }
+
+        private class CountdownSection
+        {
+            public static CountdownSection Days = new CountdownSection("days", "Days", d => d.Days, "#00");
+            public static CountdownSection Hours = new CountdownSection("hours", "Hrs", d => d.Hours, "00");
+            public static CountdownSection Minutes = new CountdownSection("minutes", "Mins", d => d.Minutes, "00");
+            public static CountdownSection Seconds = new CountdownSection("seconds", "Secs", d => d.Seconds, "00");
+
+            public readonly string Identifier;
+            public readonly string DisplayName;
+            public readonly Func<TimeSpan, int> TimeSelector;
+            public readonly string FormatString;
+
+            private CountdownSection(string identifier, string displayName, Func<TimeSpan, int> timeSelector, string formatString)
+            {
+                Identifier = identifier;
+                DisplayName = displayName;
+                TimeSelector = timeSelector;
+                FormatString = formatString;
+            }
+        }
+        
+    }
+}
---
+++ Grow.Server/wwwroot/js/jquery.eventCountDown.js
@@ -0,0 +1,54 @@
+﻿var timeinterval;
+
+$.fn.eventCountDown = function () {
+
+	var targetDateString = $(this).attr("asp-datetime");
+    if (!targetDateString)
+        return;
+
+    var targetDate = new Date(targetDateString);
+    targetDate.setTime(targetDate.getTime() + targetDate.getTimezoneOffset() * 60 * 1000);
+
+    initializeClock(this, targetDate);
+
+    return this;
+};
+
+function getTimeRemaining(endtime) {
+    var t = endtime.getTime() - Date.now();
+    var seconds = Math.floor((t / 1000) % 60);
+    var minutes = Math.floor((t / 1000 / 60) % 60);
+    var hours = Math.floor((t / (1000 * 60 * 60)) % 24);
+    var days = Math.floor(t / (1000 * 60 * 60 * 24));
+    return {
+        'total': t,
+        'days': days,
+        'hours': hours,
+        'minutes': minutes,
+        'seconds': seconds
+    };
+}
+
+function initializeClock(clock, endtime) {
+    if (!clock || !endtime) return;
+    var daysSpan = clock.querySelector('.days');
+    var hoursSpan = clock.querySelector('.hours');
+    var minutesSpan = clock.querySelector('.minutes');
+    var secondsSpan = clock.querySelector('.seconds');
+
+    function updateClock() {
+        var t = getTimeRemaining(endtime);
+
+        daysSpan.innerHTML = t.days;
+        hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
+        minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
+        secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
+
+        if (t.total <= 0) {
+            clearInterval(timeinterval);
+		}
+	}
+
+	updateClock();
+    var timeinterval = setInterval(updateClock, 1000);
+}
